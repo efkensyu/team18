56c9464feaeebe79648df2a30d8d32f746f7672c
kato <yur_kato@bold.ne.jp>
2026-05-20 16:57:39 +0900

    kato

--- src/main/java/com/example/demo/team18rental/Team18rKensaku.java
+++ src/main/java/com/example/demo/team18rental/Team18rKensaku.java
@@ -1,16 +1,17 @@
 package com.example.demo.team18rental;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
+@Controller
 public class Team18rKensaku {
 	@GetMapping("/team18kensaku") 
 	public String a() {
-		return "team18kensakuin";
+		return "team18rental/team18kensakuin";
 	}
 	@PostMapping("/team18kensaku")
 	public String send(@ModelAttribute KensakuForm kensakuForm) {
-	return "team18kensakuout";
+	return "team18rental/team18kensakuout";
 }
 }
