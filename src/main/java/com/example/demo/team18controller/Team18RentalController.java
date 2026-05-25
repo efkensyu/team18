4152586bdcd4e56b633931baaf823438b150935b
kato <yur_kato@bold.ne.jp>
2026-05-25 17:45:52 +0900

    kato

--- src/main/java/com/example/demo/team18controller/Team18RentalController.java
+++ src/main/java/com/example/demo/team18controller/Team18RentalController.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -24,14 +23,15 @@ public class Team18RentalController{
 	private final Team18StatusRepository tsr;
 	private final Team18RentalRepository trr;
 //	確認画面表示
-	@GetMapping("/team18rental")
+	@PostMapping("/team18rental")
 	public String rental (@RequestParam Integer bookId, Model model) {
+	 
 		Team18BookEntity book = trr.findByBookIdEquals(bookId);
 		model.addAttribute("book",book);
 		return "team18rental/team18rentalconfirm";
 	}
 //	確定画面
-	@PostMapping("/team18rental")
+	@PostMapping("/team18rentalresult")
 	public String go (@RequestParam Integer bookId,HttpSession session){
 		
 		Team18UserEntity user =(Team18UserEntity)
@@ -43,7 +43,7 @@ public class Team18RentalController{
 		log.setUserId(user.getUserId());
 		log.setRentStart(LocalDate.now());
 		tsr.save(log);		
-		return "/team18menu";
+		return "team18rental/team18rentalresult";
 	}
 }
 
