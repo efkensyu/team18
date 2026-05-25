23aacd50c71793cde8fa01ab8d41a963b5bebe62
omoto <j_omoto@bold.ne.jp>
2026-05-25 14:22:24 +0900

    omoto0525 バリデーションの導入

--- src/main/java/com/example/demo/team18controller/Team18LoginController.java
+++ src/main/java/com/example/demo/team18controller/Team18LoginController.java
@@ -4,11 +4,14 @@ import jakarta.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.team18entity.Team18UserEntity;
+import com.example.demo.team18form.Team18LoginForm;
+import com.example.demo.team18form.Team18RegisterForm;
 import com.example.demo.team18service.team18LoginService;
 
 import lombok.RequiredArgsConstructor;
@@ -17,37 +20,49 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class Team18LoginController {
 	private final team18LoginService tls;
-//	ログイン画面表示
+
+	//	ログイン画面表示
 	@GetMapping("/team18librarylogin")
-	public String loginpage(){
+	public String loginpage() {
 		return "team18login/team18login1";
 	}
-//	ログイン認証
+
+	//	ログイン認証
 	@PostMapping("/team18librarylogin")
-	public String login(@RequestParam String userEmail ,@RequestParam String userPass,HttpSession session,Model model) {
-		Team18UserEntity user = tls.login(userEmail, userPass);
-		if(user == null) {
-			
-			model.addAttribute("errar","メールアドレスまたはパスワードがまちがっています。");
+	public String login(@ModelAttribute Team18LoginForm team18LoginForm, HttpSession session, Model model,
+			BindingResult result) {
+		Team18UserEntity user = tls.login(team18LoginForm.getUserEmail(), team18LoginForm.getUserPass());
+		if (result.hasErrors()) {
 			return "team18login/team18login1";
 		}
-		session.setAttribute("loginUser", user);		
+		if (user == null) {
+
+			model.addAttribute("errar", "メールアドレスまたはパスワードがまちがっています。");
+			return "team18login/team18login1";
+		}
+		session.setAttribute("loginUser", user);
 		return "team18login/team18menu";
 	}
-	 @GetMapping("/team18register")
-	 public String registerPage() {
 
-	      return "team18login/Team18Register";
-	 }
+	@GetMapping("/team18register")
+	public String registerPage() {
+
+		return "team18login/Team18Register";
+	}
+
 	@PostMapping("/team18register")
-	  public String registermenu(@RequestParam String userEmail,@RequestParam String userPass,
-			  @RequestParam String userNm, Model model) {
+	public String registermenu(@ModelAttribute Team18RegisterForm team18RegisterForm, Model model,
+			BindingResult result) {
+		if (result.hasErrors()) {
+			return "team18login/Team18Register";
+		}
 
-	        tls.register(userEmail, userPass, userNm);
+		tls.register(team18RegisterForm.getUserEmail(), team18RegisterForm.getUserPass(),
+				team18RegisterForm.getUserNm());
 
-	        model.addAttribute("msg", "登録完了しました");
+		model.addAttribute("msg", "登録完了しました");
 
-	        return "team18login/team18login1";
-	    }
-	
+		return "team18login/team18login1";
+	}
+
 }
