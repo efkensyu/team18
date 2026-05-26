feda0c3a0b85d2bbbbade70f9b4455a2387c7964
omoto <j_omoto@bold.ne.jp>
2026-05-26 15:05:47 +0900

    omoto0526 称号がメニューで反映されるように変更

--- src/main/java/com/example/demo/team18/team18controller/Team18GachaController.java
+++ src/main/java/com/example/demo/team18/team18controller/Team18GachaController.java
@@ -1,11 +1,14 @@
 package com.example.demo.team18.team18controller;
 
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.team18.team18entity.Team18TitleEntity;
+import com.example.demo.team18.team18entity.Team18UserEntity;
 import com.example.demo.team18.team18service.team18GachaService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,10 +23,11 @@ public class Team18GachaController {
 		return "team18/team18gacha/Team18GachaPage";
 	}
 	@PostMapping("/team18gacha")
-	public String gacharesult(Model model) {
+	public String gacharesult(Model model, HttpSession session) {
 		String rarityname;
 		Team18TitleEntity result = tgs.gacha();
 		String name= result.getTitleNm();
+		long titleid = result.getTitleId();
 		int rarity= result.getRarity();
 		if(rarity == 1) {
 			rarityname = "R";
@@ -32,6 +36,11 @@ public class Team18GachaController {
 		}else {
 			rarityname = "SSR";
 		}
+		Team18UserEntity gachauser = (Team18UserEntity)session.getAttribute("loginUser");
+		
+		tgs.insert(titleid, gachauser.getUserEmail());
+		gachauser.setTitleId(titleid);
+		session.setAttribute("loginUser", gachauser);
 		
 		model.addAttribute("rarityname",rarityname);
 		model.addAttribute("name",name);
